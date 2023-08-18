4cd8fdc822aa48e7f530c5bf0f90147e7e5d9241
Pradeep Kumar Gupta <120917855+Pradeep-Gupta-96@users.noreply.github.com>
2023-08-18 17:28:41 +0530

    code added

--- src/Component/Navbar/AdminNavbar.js
+++ src/Component/Navbar/AdminNavbar.js
@@ -131,7 +131,9 @@ export default function AdminNavbar() {
       {
         text: "Dashboard",
         icons: <HomeIcon />,
-      
+        forNavigation: () => {
+          navigation('/emplyeeDashboard')
+        }
       },
     ]
   }
